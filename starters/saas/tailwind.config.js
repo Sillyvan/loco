f5b312cef7cda1a3af466a69e7f98c78afc303a4
Silvan <38138071+Sillyvan@users.noreply.github.com>
2024-10-18 22:12:25 +0200

    add base files for tailwind

---
+++ starters/saas/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./assets/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
+}
